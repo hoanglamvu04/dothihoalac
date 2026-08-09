1a82a3f8ab08726c6dab5fe8886fa061f3ee4532
hoanglamvu04 <74477176+hoanglamvu04@users.noreply.github.com>
2026-08-10 00:44:15 +0700

    feat(content): expose admin article detail endpoint

--- server/src/modules/articles/article.admin.routes.js
+++ server/src/modules/articles/article.admin.routes.js
@@ -1,12 +1,16 @@
 import { Router } from 'express';
 import * as c from './article.controller.js';
+import { getAdminArticleDetail } from './article.admin-detail.js';
 import { requireAuth } from '../../middlewares/auth.middleware.js';
 import { requirePermission } from '../../middlewares/role.middleware.js';
 import { PERMISSIONS } from '../../constants/permissions.js';
 import { validate } from '../../middlewares/validate.middleware.js';
 import { articleBodySchema } from './article.validation.js';
 import asyncHandler from '../../utils/asyncHandler.js';
+import { sendSuccess } from '../../utils/apiResponse.js';
+
 const r = Router();
+
 r.use(
   requireAuth,
   requirePermission(
@@ -15,7 +19,17 @@ r.use(
     PERMISSIONS.MANAGE_SYSTEM,
   ),
 );
+
 r.get('/', asyncHandler(c.adminList));
+r.get(
+  '/:id',
+  asyncHandler(async (req, res) => (
+    sendSuccess(res, {
+      data: await getAdminArticleDetail(req.params.id),
+    })
+  )),
+);
 r.post('/', validate(articleBodySchema), asyncHandler(c.adminCreate));
 r.patch('/:id', validate(articleBodySchema), asyncHandler(c.adminUpdate));
+
 export default r;
