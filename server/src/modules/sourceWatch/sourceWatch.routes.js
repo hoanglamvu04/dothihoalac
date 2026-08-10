c02ea1d4b875cfb7a7dc1d3cef900cdee47a0184
hoanglamvu04 <74477176+hoanglamvu04@users.noreply.github.com>
2026-08-10 14:46:39 +0700

    add source watch admin routes

---
+++ server/src/modules/sourceWatch/sourceWatch.routes.js
@@ -0,0 +1,28 @@
+import { Router } from 'express';
+
+import { PERMISSIONS } from '../../constants/permissions.js';
+import { requireAuth } from '../../middlewares/auth.middleware.js';
+import { requirePermission } from '../../middlewares/role.middleware.js';
+import asyncHandler from '../../utils/asyncHandler.js';
+import * as controller from './sourceWatch.controller.js';
+
+const router = Router();
+
+router.use(
+  requireAuth,
+  requirePermission(
+    PERMISSIONS.CREATE_ARTICLE,
+    PERMISSIONS.EDIT_ARTICLE,
+    PERMISSIONS.MANAGE_SYSTEM,
+  ),
+);
+
+router.get('/overview', asyncHandler(controller.overview));
+router.get('/sources', asyncHandler(controller.listSources));
+router.post('/sources', asyncHandler(controller.createSource));
+router.patch('/sources/:id', asyncHandler(controller.updateSource));
+router.post('/sources/:id/check', asyncHandler(controller.checkSource));
+router.get('/items', asyncHandler(controller.listItems));
+router.patch('/items/:id/status', asyncHandler(controller.updateItemStatus));
+
+export default router;
