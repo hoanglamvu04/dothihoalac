6e722104a223d73e3f23e6b17b5afbca77e02c00
hoanglamvu04 <74477176+hoanglamvu04@users.noreply.github.com>
2026-08-12 17:31:11 +0700

    mount upgraded public header navigation

--- client/src/components/layout/PublicLayout.jsx
+++ client/src/components/layout/PublicLayout.jsx
@@ -1,11 +1,13 @@
 import { Outlet } from 'react-router-dom';
 import SiteHeader from './SiteHeader';
+import HeaderNavigationUpgrade from './HeaderNavigationUpgrade';
 import SiteFooter from './SiteFooter';
 
 export default function PublicLayout() {
   return (
     <div className="app-shell">
       <SiteHeader />
+      <HeaderNavigationUpgrade />
       <main className="main-content"><Outlet /></main>
       <SiteFooter />
     </div>
