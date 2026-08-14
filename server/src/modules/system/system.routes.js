88706564b70fb55ea1667a25e42cead30fe82a18
hoanglamvu04 <74477176+hoanglamvu04@users.noreply.github.com>
2026-08-14 15:46:29 +0700

    add public advertising metrics routes

--- server/src/modules/system/system.routes.js
+++ server/src/modules/system/system.routes.js
@@ -1,7 +1,17 @@
 import { Router } from 'express';
-import { page, banners } from './system.controller.js';
+import {
+  page,
+  banners,
+  bannerImpression,
+  bannerClick,
+} from './system.controller.js';
 import asyncHandler from '../../utils/asyncHandler.js';
+
 const r = Router();
+
 r.get('/pages/:slug', asyncHandler(page));
 r.get('/banners', asyncHandler(banners));
+r.post('/banners/:id/impression', asyncHandler(bannerImpression));
+r.post('/banners/:id/click', asyncHandler(bannerClick));
+
 export default r;
