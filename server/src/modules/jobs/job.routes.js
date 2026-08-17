59ed4242bcdb3e552b25adba9f41dc672455254e
hoanglamvu04 <74477176+hoanglamvu04@users.noreply.github.com>
2026-08-17 12:11:55 +0700

    allow privileged preview of pending job posts

--- server/src/modules/jobs/job.routes.js
+++ server/src/modules/jobs/job.routes.js
@@ -1,13 +1,15 @@
 import { Router } from 'express';
 import * as c from './job.controller.js';
-import { requireAuth } from '../../middlewares/auth.middleware.js';
+import { optionalAuth, requireAuth } from '../../middlewares/auth.middleware.js';
 import { validate } from '../../middlewares/validate.middleware.js';
+import { servePendingContentPreview } from '../contents/pendingContentPreview.middleware.js';
 import { createSchema, updateSchema, idSchema } from './job.validation.js';
 import asyncHandler from '../../utils/asyncHandler.js';
 const r = Router();
+const pendingPreview = servePendingContentPreview('job');
 r.get('/', asyncHandler(c.list));
 r.post('/', requireAuth, validate(createSchema), asyncHandler(c.create));
 r.patch('/:id', requireAuth, validate(updateSchema), asyncHandler(c.update));
 r.post('/:id/submit', requireAuth, validate(idSchema), asyncHandler(c.submit));
-r.get('/:slug', asyncHandler(c.detail));
+r.get('/:slug', optionalAuth, asyncHandler(pendingPreview), asyncHandler(c.detail));
 export default r;
