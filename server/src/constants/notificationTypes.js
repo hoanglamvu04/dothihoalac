647384ce747750e8937a45851f1f49d89cd16a41
hoanglamvu04 <74477176+hoanglamvu04@users.noreply.github.com>
2026-08-18 10:47:33 +0700

    expand notification event types

--- server/src/constants/notificationTypes.js
+++ server/src/constants/notificationTypes.js
@@ -2,6 +2,7 @@ export const NOTIFICATION_TYPES = Object.freeze({
   POST_APPROVED: 'post_approved',
   POST_REJECTED: 'post_rejected',
   POST_NEEDS_REVISION: 'post_needs_revision',
+  POST_PUBLISHED: 'post_published',
   NEW_REACTION: 'new_reaction',
   NEW_COMMENT: 'new_comment',
   COMMENT_REPLY: 'comment_reply',
@@ -9,8 +10,11 @@ export const NOTIFICATION_TYPES = Object.freeze({
   NEW_FOLLOWER: 'new_follower',
   LISTING_EXPIRING: 'listing_expiring',
   LISTING_EXPIRED: 'listing_expired',
+  JOB_EXPIRING: 'job_expiring',
+  JOB_EXPIRED: 'job_expired',
   REPORT_RESOLVED: 'report_resolved',
   ACCOUNT_WARNING: 'account_warning',
   SYSTEM_NOTICE: 'system_notice',
 });
+
 export const NOTIFICATION_TYPE_VALUES = Object.values(NOTIFICATION_TYPES);
