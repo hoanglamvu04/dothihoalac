b70a5283803d2e656c46c18a5deefe1490136308
hoanglamvu04 <74477176+hoanglamvu04@users.noreply.github.com>
2026-08-19 15:51:19 +0700

    perf: route taxonomy bootstrap request

--- server/src/modules/taxonomy/taxonomy.routes.js
+++ server/src/modules/taxonomy/taxonomy.routes.js
@@ -1,7 +1,8 @@
 import { Router } from 'express';
-import { categories, tags, areas } from './taxonomy.controller.js';
+import { bootstrap, categories, tags, areas } from './taxonomy.controller.js';
 import asyncHandler from '../../utils/asyncHandler.js';
 const r = Router();
+r.get('/bootstrap', asyncHandler(bootstrap));
 r.get('/categories', asyncHandler(categories));
 r.get('/tags', asyncHandler(tags));
 r.get('/areas', asyncHandler(areas));
