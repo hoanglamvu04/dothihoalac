0874a5f566434f6427030f8c0700d18102efd5ff
hoanglamvu04 <74477176+hoanglamvu04@users.noreply.github.com>
2026-08-19 15:51:27 +0700

    perf: use combined taxonomy bootstrap API

--- client/src/api/taxonomy.api.js
+++ client/src/api/taxonomy.api.js
@@ -1,6 +1,7 @@
 import { api, unwrap } from './http';
 
 export const taxonomyApi = {
+  bootstrap: async (config = {}) => unwrap(await api.get('/taxonomy/bootstrap', config)),
   categories: async (params = {}) => unwrap(await api.get('/taxonomy/categories', { params })),
   tags: async (params = {}) => unwrap(await api.get('/taxonomy/tags', { params })),
   areas: async (params = {}) => unwrap(await api.get('/taxonomy/areas', { params })),
